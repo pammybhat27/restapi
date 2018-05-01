794d9101014ce1f2bbf9036927230f45220e3dba
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-05-01 15:58:02 +1000

    First Commit
    Exercise for Rest api

---
+++ ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using ContactManager.Models;
+using ContactManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        //Service Created to handle 
+        private ContactRepository contactRepository;
+
+        public ContactController() {
+            this.contactRepository = new ContactRepository();
+        }
+
+        //Using the service to do the work
+        public Contact[] Get() {
+            return contactRepository.GetAllContacts();
+        }
+
+        public HttpResponseMessage Post(Contact contact) {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+            
+            return response;
+        }
+    }
+}
